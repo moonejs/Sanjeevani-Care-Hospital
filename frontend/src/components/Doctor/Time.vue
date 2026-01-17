d3318ef47f4fb76815f0b31eefccbc9680e9d78d
Litesh <liteshsharma9466@gmail.com>
2026-01-17 12:00:47 +0530

    Timer added

---
+++ frontend/src/components/Doctor/Time.vue
@@ -0,0 +1,43 @@
+<script setup>
+    import { ref, onMounted, onUnmounted, computed } from 'vue'
+
+    const now = ref(new Date())
+    let timer = null
+    onMounted(()=>{
+        timer=setInterval(()=>{
+            now.value=new Date()
+        },1000)
+    })
+    
+    onUnmounted(()=>{
+        clearInterval(timer)
+    })
+
+    const formattedDate=computed(()=>{
+        return now.value.toLocaleDateString('en-In',{
+            weekday: 'long',
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric'
+        })
+    })
+    const formattedTime = computed(()=>{
+        return now.value.toLocaleTimeString('en-In',{
+            hour: 'numeric',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
+        })
+    })
+    
+
+
+</script>
+
+<template>
+  <div class="bg-info time">
+      <h4>{{ formattedTime }}</h4>
+    <h4>{{ formattedDate }}</h4>
+    
+  </div>
+</template>
--- frontend/src/views/doctor/DoctorLayoutPage.vue
+++ frontend/src/views/doctor/DoctorLayoutPage.vue
@@ -1,9 +1,11 @@
 <script setup>
     import { RouterView } from 'vue-router';
     import DoctorNavbar from '@/components/layout/Doctor/DoctorNavbar.vue';
+    import Time from '@/components/Doctor/Time.vue';
 </script>
 
 <template>
     <DoctorNavbar/>
+    <Time/>
     <RouterView/>
 </template>
